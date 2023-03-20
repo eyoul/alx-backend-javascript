698c7de62ca3f8c66c839c2733e65b81645d23c7
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-03-20 15:09:49 -0700

    Update 8-getBudgetCurrentYear.js

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,4 +1,7 @@
-import { getCurrentYear } from "./getCurrentYear";
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
