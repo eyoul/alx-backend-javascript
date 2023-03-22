d44185ea60d08ee9a4525eb9febb10d51450192b
eyoul <eyoulshimeles2@gmail.com>
2023-03-22 12:45:22 +0000

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,4 +3,4 @@ export default function getResponseFromAPI(success) {
     if (success) resolve({ status: 200, body: 'Success' });
     else reject(new Error('The fake API is not working currently'));
   });
-}//new
+}
