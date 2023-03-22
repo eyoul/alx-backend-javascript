f6302354049c8729026b15a61d2421764d70359e
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-03-22 12:34:28 -0700

    Update 6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName){
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => values.map((value) => ({
+    status: value.status,
+    value: value.status === 'fulfilled' ? value.value : String(value.reason),
+  })));   
+}
