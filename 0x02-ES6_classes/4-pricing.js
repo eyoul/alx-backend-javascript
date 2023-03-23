26a8b20a33243ad20074ac3ba230958257908b29
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-03-23 15:20:06 -0700

    Update 4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,30 +1,30 @@
 export default class Pricing {
-  constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
-  }
+	constructor(amount, currency) {
+		this._amount = amount;
+		this._currency = currency;
+	}
 
-  get amount() {
-    return this._amount;
-  }
+	get amount() {
+		return this._amount;
+	}
 
-  set amount(amount) {
-    this._amount = amount;
-  }
+	set amount(amount) {
+		this._amount = amount;
+	}
 
-  get currency() {
-    return this._currency;
-  }
+	get currency() {
+		return this._currency;
+	}
 
-  set currency(currency) {
-    this._currency = currency;
-  }
+	set currency(currency) {
+		this._currency = currency;
+	}
 
-  displayFullPrice() {
-    return `${this._amount} ${this._currency.displayFullCurrency()}`;
-  }
+	displayFullPrice() {
+		return `${this._amount} ${this._currency.displayFullCurrency()}`;
+	}
 
-  static convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
-  }
+	static convertPrice(amount, conversionRate) {
+		return amount * conversionRate;
+	}
 }
