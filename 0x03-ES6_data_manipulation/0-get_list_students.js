00a0ae0581564d429c9b06e0de24cff7d9a4c33a
eyoul <eyoulshimeles2@gmail.com>
2023-03-27 20:41:19 +0000

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -6,8 +6,8 @@ export default function getListStudents() {
   });
 
   return [
-    createStudent( 1, 'Guillaume', 'San Francisco' ),
-    createStudent( 2, 'James', 'Columbia' ),
-    createStudent( 5, 'Serena', 'San Francisco'),
-    ];
+    createStudent(1, 'Guillaume', 'San Francisco' ),
+    createStudent(2, 'James', 'Columbia' ),
+    createStudent(5, 'Serena', 'San Francisco'),
+   ];
 }
