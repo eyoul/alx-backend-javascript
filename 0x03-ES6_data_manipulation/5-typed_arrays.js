d592b4f1608e1c8204fef57e3c82ee1bf055604f
eyoul <eyoulshimeles2@gmail.com>
2023-03-28 18:11:29 +0000

    5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,4 @@
-export default function createInt8TypedArray() {
+export default function createInt8TypedArray(length, position, value) {
   if (position >= length) throw Error('Position outside range');
   const buffer = new ArrayBuffer(length);
   const newView = new DataView(buffer, 0, length);
