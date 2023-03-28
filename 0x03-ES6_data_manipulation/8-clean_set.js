67a7ef30213e22ac1b9659d93d2415948881d72c
eyoul <eyoulshimeles2@gmail.com>
2023-03-28 18:51:12 +0000

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,10 @@
-export default function hasValuesFromArray(set, arr) {
-  for (const el of arr){
-    if (!set.has(el)) return false;
+export default function cleanSet(set, startString) {
+  let res = '';
+  if (!startString || !startString.length) return res;
+  for (const el of set) {
+    if (el && el.startsWith(startString)) {
+      res += `${el.slice(startString.length)}-`;
+    }
   }
-  return true;
+  return res.slice(0, res.length - 1);
 }
