1f7e6266c14319b9a92f5bb6c64042b1781804c7
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-16 02:31:17 -0700

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 
-function constStudents(path) {
+function countStudents(path) {
   try {
     const fileData = fs.readFileSync(path, 'utf8');
-    const lines = fileData.split('\n').filter(line => line !=='');
+    const lines = fileData.split('\n').filter(line => line !== '');
     const headers = lines[0].split(',');
     const fieldCounts = {};
+
     for (let i = 1; i < lines.length; i++) {
       const values = lines[i].split(',');
       if (values.length === headers.length) {
@@ -17,23 +18,24 @@ function constStudents(path) {
             fieldCounts[header].count++;
             fieldCounts[header].students.push(value);
           } else {
-            fieldCounts[header] = { count: 1, students: [value]};
+            fieldCounts[header] = { count: 1, students: [value] };
           }
         }
       }
     }
 
-    const totalCount = lines.length -1;
+    const totalCount = lines.length - 1;
     console.log(`Number of students: ${totalCount}`);
 
     for (const header in fieldCounts) {
       const count = fieldCounts[header].count;
-      const students = fieldCounts[header].students.join(',');
+      const students = fieldCounts[header].students.join(', ');
       console.log(`Number of students in ${header}: ${count}. List: ${students}`);
     }
+
   } catch (error) {
     throw new Error('Cannot load the database');
   }
 }
 
-module.exports = constStudents;
+module.exports = countStudents;
