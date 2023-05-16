97512f84ee6c46760e2b93fd4ba0f5cf7d08a1b8
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-16 01:41:04 -0700

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
     process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
-});
+  });
 } else {
   process.stdin.on('data', (data) => {
     process.stdout.write(`Your name is: ${data.toString()}`);
@@ -12,5 +12,5 @@ if (process.stdin.isTTY) {
   });
   process.on('exit', () => {
     process.stdout.write('This important software is now closing\n');
- });
+  });
 }
