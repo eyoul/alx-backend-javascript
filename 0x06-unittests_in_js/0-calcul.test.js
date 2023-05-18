7a2191a80c253de31c473b6b9f5abc30c07ac65f
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-18 01:03:42 -0700

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,11 +1,30 @@
 const assert = require('assert');
+const { it, describe } = require('mocha')
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function() {
-  idText('should return the sum of two rounded number', function() {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6)
+  it(`checking if number round`, function() {
+    const res = calculateNumber(1, 2);
+    assert.strictEqual(res, 3);
+  });
+  it(`checking if number round`, function() {
+    const res = calculateNumber(1.4, 2.2);
+    assert.strictEqual(res, 3);
+  });
+  it(`checking if number round`, function() {
+    const res = calculateNumber(1.6, 2.7);
+    assert.strictEqual(res, 5);
+  });
+  it(`checking if number round`, function() {
+    const res = calculateNumber(0, 0);
+    assert.strictEqual(res, 0);
+  });
+  it(`checking if number round`, function() {
+    const res = calculateNumber(-1.6, -1.7);
+    assert.strictEqual(res, -4);
+  });
+  it(`checking if number round`, function() {
+    const res = calculateNumber(-1.4, -1.3);
+    assert.strictEqual(res, -2);
   });
 });
