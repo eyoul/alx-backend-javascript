c0428e0bf9b5da01a78abfba135e0fd69dba6ae2
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-18 07:23:51 -0700

    Update 1-calcul.test.js

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,38 +1,31 @@
-const assert = require('assert');
-const calculateNumber = require('./1-calcul');
+const assert = require("assert");
+const { it, describe } = require("mocha");
+const calculateNumber = require("./1-calcul");
 
-describe('calculateNumber', function() {
-  describe('SUM', function() {
-    it('should return the sum of two rounded numbers', function() {
-      assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
-      assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
-      assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
-    });
-  });
+describe("calculateNumber()", function() {
 
-  describe('SUBTRACT', function() {
-    it('should return the difference of two rounded numbers', function() {
-      assert.strictEqual(calculateNumber('SUBTRACT', 1, 3.7), -3);
-      assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 3.7), -3);
-      assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 3.7), -2);
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber("SUM", 1, 2);
+      assert.strictEqual(res, 3);
     });
-  });
-
-  describe('DIVIDE', function() {
-    it('should return the quotient of two rounded numbers', function() {
-      assert.strictEqual(calculateNumber('DIVIDE', 1, 2), 0.5);
-      assert.strictEqual(calculateNumber('DIVIDE', 1.2, 3.7), 0.3);
-      assert.strictEqual(calculateNumber('DIVIDE', 1.5, 3.7), 0.4);
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber("SUBTRACT", 1.4, 2.2);
+      assert.strictEqual(res, -1);
     });
-
-    it('should return Error when the second argument is 0', function() {
-      assert.strictEqual(calculateNumber('DIVIDE', 1, 0), 'Error');
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber("SUBTRACT", 4.9, 2.7);
+      assert.strictEqual(res, 2);
     });
-  });
-
-  describe('Invalid type', function() {
-    it('should return Invalid type message', function() {
-      assert.strictEqual(calculateNumber('INVALID', 1, 2), 'Invalid type');
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber("DIVIDE", 4, 2);
+      assert.strictEqual(res, 2);
     });
-  });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber("DIVIDE", 1.7, 0);
+      assert.strictEqual(res, "Error");
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber("DIVIDE", 1.4, 4.6);
+      assert.strictEqual(res, 0.2);
+    });
 });
