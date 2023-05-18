c77ca19867b80c973433a4b1dc321f54695a33e4
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-05-18 07:23:16 -0700

    Update 1-calcul.js

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,19 +1,23 @@
 function calculateNumber(type, a, b) {
-    const anum = Math.round(a);
-    const bnum = Math.round(b);
-    switch (type) {
-      case 'SUM':
-        return anum + bnum;
-      case 'SUBTRACT':
-        return anum - bnum;
-      case 'DIVIDE':
-        if (bnum === 0) {
-          return 'Error';
-        }
-        return anum / bnum;
-      default:
-        return 'Invalid type';
+  const anum = Math.round(a);
+  const bnum = Math.round(b);
+  let c = 0;
+  switch (type) {
+  case 'SUM':
+c = anum + bnum;
+break;
+  case 'SUBTRACT':
+c = anum - bnum;
+break;
+  case 'DIVIDE':
+    if (bnum === 0) {
+          c = "Error";
+    } else {
+          c = anum / bnum;
     }
+    break;
   }
-  
-  module.exports = calculateNumber;
+  return c;
+}
+
+module.exports = calculateNumber;
